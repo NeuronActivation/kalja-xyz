3faa830f2d99dc1ec4b75d411e792d308b83a26a
jervw <vuolajere@gmail.com>
2023-12-17 21:39:37 +0200

    Final piece of the project setup

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Drinking game";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.nodePackages.pnpm];
+        buildInputs = with pkgs; [
+          openssl
+          nodejs_18
+        ];
+      };
+    });
+}
