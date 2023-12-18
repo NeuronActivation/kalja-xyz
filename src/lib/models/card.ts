7a3e2853e02e67d95b8533d5397e017afb40dd23
viirret <viirretvaltteri@gmail.com>
2023-12-18 15:25:31 +0200

    Add initial core game logic

---
+++ src/lib/models/card.ts
@@ -0,0 +1,9 @@
+// The original source refers this as "question".
+// However I think card is more appropriate.
+
+export declare type GameCard = {
+	title: string;
+	description: string;
+	timedEvent: boolean;
+	targetingPlayer: boolean;
+};
