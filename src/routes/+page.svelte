0059dd31339350eef043eda2e01ccf42a09d72d0
Jere Vuola <jervw@pm.me>
2024-02-26 16:39:50 +0200

    Improve UI with PicoCSS Styling (#11)
    Some basic styling with PicoCSS components. Also enabled formatting for
    .svelte files – I forgot to include that earlier. Lots of changes, but
    that's just because of the formatting addition.
    
    TLDR: added style tags and the player removal function.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { base } from "$app/paths";
+	import { base } from '$app/paths';
 	import { cardsInGame } from '$lib/constants/cardsInGame';
 	import type { GameCard } from '$lib/models/card';
 
@@ -21,13 +21,13 @@
 		GAME_OVER = 'game_over'
 	}
 
-    type GameEvent = {
-        title: string,
-        person: string,
-        startingIndex: number,
-        ended: boolean
-    }
-    let events: GameEvent[] = [];
+	type GameEvent = {
+		title: string;
+		person: string;
+		startingIndex: number;
+		ended: boolean;
+	};
+	let events: GameEvent[] = [];
 
 	type Player = {
 		id: number;
@@ -77,6 +77,10 @@
 		gameContext.players = [...gameContext.players, newPlayer];
 	}
 
+	function removePlayer(playerId: number) {
+		gameContext.players = gameContext.players.filter((player) => player.id !== playerId);
+	}
+
 	function handleKeyPress(event: KeyboardEvent) {
 		if (event.key === 'Enter') {
 			addPlayer(playerName);
@@ -89,45 +93,44 @@
 		currentPlayerIndex += 1;
 
 		// Go to game over if out of cards.
-        // Card index doesn't have to be updated since it's always updated in startGame().
-        if (currentCardIndex >= cardsInGame) {
-		    changeGameState(GameStates.GAME_OVER);
-            return;
-        }
+		// Card index doesn't have to be updated since it's always updated in startGame().
+		if (currentCardIndex >= cardsInGame) {
+			changeGameState(GameStates.GAME_OVER);
+			return;
+		}
 
 		if (currentPlayerIndex >= gameContext.players.length) {
 			currentPlayerIndex = 0;
 		}
 
 		// Clearing ended events.
-		events = events.filter(item => !item.ended);
+		events = events.filter((item) => !item.ended);
 
 		// Last card so all events automatically end.
 		if (currentCardIndex + 1 >= cardsInGame) {
 			events.forEach((event) => {
 				event.ended = true;
-			})
+			});
 			return;
 		}
 
 		// Event ends normally.
-		events.forEach(event => {
-            if (currentPlayerIndex === event.startingIndex) {
-                event.ended = true;
-            }
-        });
+		events.forEach((event) => {
+			if (currentPlayerIndex === event.startingIndex) {
+				event.ended = true;
+			}
+		});
 
 		// Add a new event.
-        if (gameCards[currentCardIndex].timedEvent)
-        {
-            let event: GameEvent = {
-                title: gameCards[currentCardIndex].title,
-                person: gameContext.players[currentPlayerIndex].name,
-                startingIndex: currentPlayerIndex,
-                ended: false
-            }
-            events.push(event);
-        }
+		if (gameCards[currentCardIndex].timedEvent) {
+			let event: GameEvent = {
+				title: gameCards[currentCardIndex].title,
+				person: gameContext.players[currentPlayerIndex].name,
+				startingIndex: currentPlayerIndex,
+				ended: false
+			};
+			events.push(event);
+		}
 	}
 
 	function getTarget(cardIndex: number, playerIndex: number): string {
@@ -145,58 +148,120 @@
 	}
 </script>
 
-{#if gameContext.state === GameStates.LOBBY}
-	<h1>Juomapeli</h1>
-	<button on:click={() => changeGameState(GameStates.ADDING_PLAYERS)}>Aloita</button>
-{:else if gameContext.state === GameStates.ADDING_PLAYERS}
-	<h1>Lisää pelaajien nimet</h1>
+<div class="game-container">
+	{#if gameContext.state === GameStates.LOBBY}
+		<h1>Santerin Juomapeli v2</h1>
+		<button class="button" on:click={() => changeGameState(GameStates.ADDING_PLAYERS)}
+			>Aloita</button
+		>
+	{:else if gameContext.state === GameStates.ADDING_PLAYERS}
+		<h2>Lisää pelaajien nimet</h2>
+		<input
+			type="text"
+			bind:value={playerName}
+			placeholder="Syötä pelaajan nimi"
+			on:keypress={handleKeyPress}
+			class="input"
+		/>
+		{#if gameContext.players.length > 0}
+			<ul class="player-list">
+				{#each gameContext.players as player (player.id)}
+					<li>
+						<button class="remove-player" on:click={() => removePlayer(player.id)}>&#x2716;</button>
+						{player.name}
+					</li>
+				{/each}
+			</ul>
+		{/if}
+		<button class="button button-green" on:click={startGame}>Aloita ryyppääminen</button>
+	{:else if gameContext.state === GameStates.PLAYING}
+		<h1 class="target">{gameContext.players[currentPlayerIndex].name}</h1>
 
-	<input
-		type="text"
-		bind:value={playerName}
-		placeholder="Syötä pelaajan nimi"
-		on:keypress={handleKeyPress}
-	/>
+		<article>
+			<h2>{gameCards[currentCardIndex].title}</h2>
+			<p>
+				{gameCards[currentCardIndex].description + getTarget(currentCardIndex, currentPlayerIndex)}
+			</p>
+		</article>
 
-	<!-- Display added players -->
-	{#if gameContext.players.length > 0}
-		<h2>Pelaajat lisätty:</h2>
-		<ul>
-			{#each gameContext.players as player (player.id)}
-				<li>{player.name}</li>
-			{/each}
-		</ul>
+		{#each events as event}
+			{#if event.ended === true}
+				<h1 class="event-text">{event.person}, voit lopetaa tehtävän {event.title}</h1>
+			{/if}
+		{/each}
+		{#if currentCardIndex + 1 < 29}
+			<button class="button" on:click={showNextCard}>Seuraava kortti</button>
+		{:else if currentCardIndex + 1 === 29}
+			<button class="button" on:click={showNextCard}>Viimeinen kortti</button>
+		{:else if currentCardIndex + 1 === 30}
+			<button class="button button-red" on:click={showNextCard}>Peli ohi</button>
+		{/if}
+		<p class="game-status">{currentCardIndex + 1}/{cardsInGame}</p>
+	{:else if gameContext.state === GameStates.GAME_OVER}
+		<h1>PELI OHI</h1>
+		<button
+			class="button"
+			on:click={() => {
+				changeGameState(GameStates.LOBBY);
+				events = [];
+			}}>Takaisin alkuun</button
+		>
 	{/if}
+</div>
 
-	<button on:click={startGame}>Aloita ryyppääminen</button>
-{:else if gameContext.state === GameStates.PLAYING}
-	<div>
-		<h2>{gameCards[currentCardIndex].title}</h2>
-		<p>Kohde: {gameContext.players[currentPlayerIndex].name}</p>
-		<p>
-			{gameCards[currentCardIndex].description + getTarget(currentCardIndex, currentPlayerIndex)}
-		</p>
+<style>
+	.game-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100vh;
+	}
 
-        {#each events as event}
-            {#if event.ended === true}
-            <h1>{event.person} voit lopetaa tehtävän {event.title}</h1>
-            {/if}
-        {/each}
+	.button-green {
+		background-color: #2ecc71;
+	}
 
-		<p>{currentCardIndex + 1}/{cardsInGame}</p>
-	</div>
-	{#if currentCardIndex + 1 < 29}
-		<button on:click={showNextCard}>Seuraava kortti</button>
-	{:else if currentCardIndex + 1 === 29}
-		<button on:click={showNextCard}>Viimeinen kortti</button>
-	{:else if currentCardIndex + 1 === 30}
-		<button on:click={showNextCard}>Peli ohi</button>
-	{/if}
-{:else if gameContext.state === GameStates.GAME_OVER}
-	<h1>PELI OHI</h1>
-	<button on:click={() => {
-		changeGameState(GameStates.LOBBY);
-		// Clearing remaining events, if any.
-		events = [];
-	}}>Takaisin alkuun</button>
-{/if}
+	.player-list {
+		padding-left: 0;
+	}
+
+	.player-list li {
+		list-style-type: none;
+	}
+
+	.remove-player {
+		all: unset;
+		background-color: transparent;
+		cursor: pointer;
+		font-size: 14px;
+		color: #666;
+		transition: color 0.2s ease;
+	}
+
+	.remove-player:hover {
+		color: #e74c3c;
+	}
+
+	input {
+		width: 50%;
+		max-width: 300px;
+		padding: 0.5rem 1rem;
+		border-radius: 0.5rem;
+		outline: none;
+	}
+
+	input:focus {
+		border-color: #2980b9;
+	}
+
+	article {
+		width: 50%;
+		max-width: 600px;
+	}
+
+	.game-status {
+		margin-top: 1rem;
+	}
+</style>
