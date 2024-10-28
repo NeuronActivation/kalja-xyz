6abbb129cbd0eb3a56324127b7451c535aeeffda
viirret <viirretvaltteri@gmail.com>
2024-10-29 01:29:43 +0200

    Respond to review comment

--- src/lib/components/End.svelte
+++ src/lib/components/End.svelte
@@ -4,7 +4,22 @@
 </script>
 
 <h1>{$t('game-over')}</h1>
-<button on:click={gameStore.startGame}>{$t('replay')}</button>
-<button on:click={gameStore.reset} style="margin-top: 1rem;">
-	{$t('back-to-start')}
-</button>
+
+<div class="button-container">
+	<button class="pico-background-jade-500" on:click={gameStore.startGame}>{$t('replay')}</button>
+	<button class="pico-background-red-500" on:click={gameStore.reset}>{$t('back-to-start')}</button>
+</div>
+
+<style>
+	.button-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 1rem;
+	}
+
+	.button-container button {
+		margin: 0.5rem 0;
+		padding: 0.5rem 1rem;
+	}
+</style>
