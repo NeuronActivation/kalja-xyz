ff245cf31706c98b230924c41876b54c7ff32156
jervw <jervw@pm.me>
2025-01-28 19:19:51 +0200

    Add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,36 @@
+<main>
+	<article>
+		<div class="error-content">
+			<hgroup>
+				<h1>404</h1>
+				<h2>Page not found</h2>
+			</hgroup>
+			<p>The page you're looking for doesn't exist.</p>
+			<a href="/" role="button">Return Home</a>
+		</div>
+	</article>
+</main>
+
+<style>
+	.error-content {
+		text-align: center;
+		margin: auto;
+		padding: 2rem;
+	}
+
+	main {
+		min-height: 100vh;
+		display: grid;
+		place-items: center;
+	}
+
+	h1 {
+		font-size: 4rem;
+		margin-bottom: 0;
+	}
+
+	h2 {
+		margin-top: 0;
+		opacity: 0.8;
+	}
+</style>
