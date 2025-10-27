64920ee5e3517afc7ebeffc33f9203bb0baeb9a5
Valtteri Viirret <viirretvaltteri@gmail.com>
2025-10-27 14:28:08 +0200

    Accessibility fixes (#125)
    - Added label and title for the select element in header.
    - Added more contrast for the background of that same element.
    - Same background for the "resetbutton" in header.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -21,14 +21,20 @@
 	}
 </script>
 
-<select class="language pico-background-azure-500" bind:value={$locale} on:change={changeLanguage}>
+<select
+	class="language pico-background-azure-700"
+	bind:value={$locale}
+	on:change={changeLanguage}
+	aria-label={$t('select-language')}
+	title={$t('language-selection')}
+>
 	{#each Object.values(Language) as language}
 		<option value={language}>{language}</option>
 	{/each}
 </select>
 
 <button
-	class="reset pico-background-azure-500"
+	class="reset pico-background-azure-700"
 	on:click={gameStore.reset}
 	title={$t('back-to-start')}
 >
